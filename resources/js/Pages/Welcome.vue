fd137671ed356cb307d2834262abd845bec80997
iLoveForLoop <jefbay110@gmail.com>
2025-02-04 07:51:51 +0800

    final

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { Link } from '@inertiajs/vue3';
 import { ref } from 'vue'
 
 const message = ref("This is an Inertia App.")
@@ -9,4 +10,6 @@ const message = ref("This is an Inertia App.")
 
     <h1>Hello World!</h1>
     <div>{{ message }}</div>
+    <Link class="underline text-blue-700" href="/categories">Go to /categories</Link> <br>
+    <Link class="underline text-blue-700" href="/products">Go to /products</Link>
 </template>
