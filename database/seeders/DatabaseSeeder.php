b1f3360d99561ac671c207b96a6e9c54f308a5a9
iLoveForLoop <jefbay110@gmail.com>
2025-02-05 20:19:48 +0800

    small issue fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,8 +21,8 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $this->call([
-            ProductSeeder::class,
             CategorySeeder::class,
+            ProductSeeder::class,
         ]);
 
 
